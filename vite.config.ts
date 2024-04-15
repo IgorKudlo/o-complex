b5c7e9c45f8d5bd96003b4d19921d6cb02788637
igor.kudlo <igoreklida@gmail.com>
2024-04-15 23:31:55 +0300

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/IgorKudlo/o-complex",
   plugins: [react()],
   server: {
     open: true,
